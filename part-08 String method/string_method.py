e72b43dc3849defccd5902b2a53a05caee2cf880
Raushan <saty.k.0021@gmail.com>
2024-06-17 12:16:10 +0530

    first commit

---
+++ part-08 String method/string_method.py
@@ -0,0 +1,17 @@
+text = "my name is satyanarayan kumar"
+
+# new_text = text.upper() #upper method
+# new_text = text.lower()    #lower method
+# new_text = text.title()      #title method
+# new_text = text.strip()      #strip method---using word space
+# new_text = text.lstrip()    #lstrip methosd remove left side space
+# new_text = text.rstrip()      #lstrip methosd remove left side space
+
+# new_text = text.find("kum")      #find the letter number
+
+new_text = text.replace("kumar", "chauhan")  # replace method
+# new_text = text.replace("k", "c")              #replace method
+
+print(new_text)
+
+new_text="chauhan" in 
